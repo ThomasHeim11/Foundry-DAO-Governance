544801f58a9012458b4aa98783fbc36f810e0104
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-07 05:26:29 +0200

    update documentation✅

--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
+/**
+ * @title TimeLock
+ * @dev This contract extends the OpenZeppelin TimelockController contract and adds additional functionality for specifying proposers and executors.
+ */
 contract TimeLock is TimelockController {
-    // minDelay is how long you have to wait before executing
-    // proposers is the list of addresses that can propose
-    // executors is the list of addresses that can execute
+    /**
+     * @dev Initializes the contract with the given minimum delay, proposers, executors, and default admin.
+     * @param minDelay The minimum delay required before executing a proposal.
+     * @param proposers The list of addresses that can propose new proposals.
+     * @param executors The list of addresses that can execute proposals.
+     */
     constructor(uint256 minDelay, address[] memory proposers, address[] memory executors)
         TimelockController(minDelay, proposers, executors, msg.sender)
     {}
